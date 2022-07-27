4732e54cc4f4a1d36c1a8f7893b2a708efbbccc7
Crowdbotics <team@crowdbotics.com>
2022-07-27 00:38:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/mysteryworks_36436/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mysteryworks_36436;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "mysteryworks_36436";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mysteryworks_36436",
+  url: "https://mysteryworks_36436.botics.co",
+  api: "https://mysteryworks_36436.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
